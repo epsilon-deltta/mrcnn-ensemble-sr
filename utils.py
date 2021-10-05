4510f4bd54713eacb4a67b0f577d64d4834e027a
epsilon-deltta <kokomong1316@gmail.com>
2021-10-05 17:08:52 +0900

    deleted window_size (the number of minibatch is over the window_size)

--- utils.py
+++ utils.py
@@ -20,7 +20,8 @@ class SmoothedValue(object):
     def __init__(self, window_size=20, fmt=None):
         if fmt is None:
             fmt = "{median:.4f} ({global_avg:.4f})"
-        self.deque = deque(maxlen=window_size)
+        # self.deque = deque(maxlen=window_size)
+        self.deque = deque()
         self.total = 0.0
         self.count = 0
         self.fmt = fmt
